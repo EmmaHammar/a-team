d4b69d1a2dc9c6fc5c514425a9f182790215ecdd
Emma Hammar <emmahammar@Emmas-MacBook-2.local>
2021-03-31 12:08:33 +0200

    move sorted array function outside printEmp function

--- ateam.js
+++ ateam.js
@@ -2,30 +2,28 @@ import {data} from './modules/fetch.mjs';
 
 data.then(data => {
     // console.log("data from fetch.mjs:", data);
-    printEmp(data)
-})
+    printEmp(data); 
+});
 
 const root = document.getElementById("root");
 const ul = document.createElement("ul");
 root.appendChild(ul);
 
 function printEmp(employees) {
-    
-    // console.log("sorterad array yngst först", employees);
-    employees.sort(function (a, b) {
-        return b.age - a.age;
-    });    
+    sortByAge(employees);
 
     for (let employee in employees) {
-        // console.log("tar den sorterade employees", employees);
-        // console.log("employees[employee]", employees[employee]);
-        // console.log("vi vill skriva ut: ", employees[employee].email);
-
         const employeeTemplate = `<li>${employees[employee].title}, ${employees[employee].name}, ${employees[employee].email}</li>`;
 
         ul.insertAdjacentHTML("afterbegin", employeeTemplate);
     }
 };
+
+function sortByAge(employees) {
+    employees.sort(function (a, b) {
+        return b.age - a.age;
+    });    
+}
 
 /*
 Använd fetch för att hämta alla anställda och skriv sedan ut som lista "ul" med varje anställd som en "li" (en rad) med titel, namn och e-mailadress. De anställda skall sorteras enligt yngst först.
